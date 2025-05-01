2a4ac95c77594fbbaace4dd48ab78ef0a1bbd1ad
minjae <mj3242@naveer.com>
2025-05-01 21:33:24 +0900

    feat: 사용자 엔티티 추가

---
+++ src/main/java/com/example/test_ex/user/domain/User.java
@@ -0,0 +1,28 @@
+package com.example.test_ex.user.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
